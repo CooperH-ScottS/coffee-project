13df84de1d2f76402ccef484bb2a1e9dc2c16e40
Cooper Hanke <cooper.hanke@gmail.com>
2018-06-26 14:50:07 -0500

    cleaned up some of the js while assisting students in class

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 "use strict";
 
+var coffees = [
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+];
+
 function renderCoffee(coffee) {
     var html = '<ul class="coffee">';
     html += '<li class="list-font col-sm-6 col-md-6 col-lg-6"><h2 class="list-font">' + coffee.name + ' <small>';
@@ -16,8 +33,6 @@ function renderCoffees(coffees) {
 }
 
 function updateCoffees(e) {
-    // e.preventDefault(); // don't submit the form, we just want to update the data
-    // console.log(e);
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
     if (e === 'all') {
@@ -59,38 +74,17 @@ function addCoffee() {
     tbody.innerHTML = renderCoffees(coffees);
 }
 
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-
 function arrangeCoffees() {
     coffees.sort(function(a, b){return a.id - b.id});
     coffees.reverse();
 }
 
-// function from previous exercise that will capitalize each word passed into addCoffee()
 function formatNewCoffee(input) {
     return input.replace(/\b\w/g, function(letter) {
         return letter.toUpperCase()
     })}
 
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
-
 var tbody = document.querySelector('#coffees');
-// var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
 
 coffees.reverse();
